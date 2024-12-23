cfb85ee4da7c6732e33f86cd2cd7f51e1d94ad31
gabriel-gpx <gp09072020@gmail.com>
2024-12-23 15:45:43 -0300

    Começo do Projeto

---
+++ capitulo-zero/src/main/java/com/wepapi/capitulo_zero/CapituloZeroProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wepapi.capitulo_zero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapituloZeroProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapituloZeroProjectApplication.class, args);
+	}
+
+}
---
+++ capitulo-zero/src/test/java/com/wepapi/capitulo_zero/CapituloZeroProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wepapi.capitulo_zero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapituloZeroProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
